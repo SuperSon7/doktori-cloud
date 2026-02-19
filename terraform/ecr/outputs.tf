8ab6b04773108da6592f827514c21abf788c9f32
Superson7 <vin990227@naver.com>
2026-02-19 23:41:09 +0900

    refactor(terraform): ECR 모듈 및 deploy IAM 정책 제거
    - terraform/ecr/ 모듈 전체 삭제 (ECR → GitHub Container Registry 전환)
    - deploy IAM에서 ECR push, SSM command 정책 제거 (미사용)

--- terraform/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_urls" {
-  description = "Map of repository name to URL"
-  value       = { for k, v in aws_ecr_repository.this : k => v.repository_url }
-}
-
-output "registry_id" {
-  description = "ECR registry ID (AWS account ID)"
-  value       = values(aws_ecr_repository.this)[0].registry_id
-}
